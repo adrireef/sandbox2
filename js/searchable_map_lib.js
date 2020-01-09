1b326b555bd8c114d031a03b4c66b6755615d071
Annalisa Minelli <annalisapg@users.noreply.github.com>
2020-01-09 11:26:49 +0100

    new dataset

--- js/searchable_map_lib.js
+++ js/searchable_map_lib.js
@@ -252,10 +252,10 @@ var SearchableMapLib = {
     SearchableMapLib.currentResults = JSON.parse(JSON.stringify(SearchableMapLib.geojsonData));
     console.log('dovrebbe aver fatto, stampo il risultato:')
     console.log(SearchableMapLib.currentResults);
-    console.log('provo la deep copy con jquery:')
-    $.extend(true, SearchableMapLib.currentResults, SearchableMapLib.geojsonData);
-    console.log('dovrebbe aver fatto, stampo il risultato (2):')
-    console.log(SearchableMapLib.currentResults);
+    //console.log('provo la deep copy con jquery:')
+    //$.extend(true, SearchableMapLib.currentResults, SearchableMapLib.geojsonData);
+    //console.log('dovrebbe aver fatto, stampo il risultato (2):')
+    //console.log(SearchableMapLib.currentResults);
 
     if(SearchableMapLib.currentPinpoint != null && address != '') {
         var point = turf.point([SearchableMapLib.currentPinpoint[1], SearchableMapLib.currentPinpoint[0]]);
