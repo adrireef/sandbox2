832d896997673fd180ee738fc078b9e99038ecbf
Annalisa Minelli <annalisapg@users.noreply.github.com>
2020-01-10 10:57:33 +0100

    restored previous version

--- js/searchable_map_lib.js
+++ js/searchable_map_lib.js
@@ -248,7 +248,14 @@ var SearchableMapLib = {
     var address = $("#search-address").val();
 
     // this is a fun hack to do a deep copy of the GeoJSON data
+    console.log('copio i punti geojson in current results..')
     SearchableMapLib.currentResults = JSON.parse(JSON.stringify(SearchableMapLib.geojsonData));
+    console.log('dovrebbe aver fatto, stampo il risultato:')
+    console.log(SearchableMapLib.currentResults);
+    //console.log('provo la deep copy con jquery:')
+    //$.extend(true, SearchableMapLib.currentResults, SearchableMapLib.geojsonData);
+    //console.log('dovrebbe aver fatto, stampo il risultato (2):')
+    //console.log(SearchableMapLib.currentResults);
 
     if(SearchableMapLib.currentPinpoint != null && address != '') {
         var point = turf.point([SearchableMapLib.currentPinpoint[1], SearchableMapLib.currentPinpoint[0]]);
@@ -265,7 +272,7 @@ var SearchableMapLib = {
  
     //-----custom filters-----
 
-    //-----Reef type filter-----
+    //-----facility type filter-----
     //filter on location type. constructing a list of OR statements based on what checkboxes are selected
     var customFilters = [];
     if ( $("#cbType1").is(':checked')) {
@@ -286,10 +293,11 @@ var SearchableMapLib = {
         filter = filter.substring(0, filter.length - 3);
         return eval(filter);
     });
-
-    //-----end Reef type filter-----
+    console.log('stampo i punti che trovo')
+    console.log(SearchableMapLib.currentResults.features)
+    //-----end facility type filter-----
     
-	//-----Reef country filter-----
+	//-----facility country filter-----
     //filter on location type. constructing a list of OR statements based on what checkboxes are selected
     var customFilters = [];
     if ( $("#cbType4").is(':checked')) {
@@ -307,18 +315,7 @@ var SearchableMapLib = {
         filter = filter.substring(0, filter.length - 3);
         return eval(filter);
     });
-    //-----end Reef country filter-----
-    
-    
-    //-----Reef usage filter-----
-    //filter on location type. constructing a list of OR statements based on what checkboxes are selected
-    var customFilters = [];
-    if ( $("#cbType6").is(':checked')) {
-      customFilters.push('r.properties["Country"] === "Italy"');
-    }
-    if ( $("#cbType7").is(':checked')) {
-      customFilters.push('r.properties["Country"] === "Croatia"');
-    }
+    //-----end facility country filter-----
 
     SearchableMapLib.currentResults.features = $.grep(SearchableMapLib.currentResults.features, function(r) {
         var filter = "";
@@ -328,8 +325,7 @@ var SearchableMapLib = {
         filter = filter.substring(0, filter.length - 3);
         return eval(filter);
     });
-    //-----end Reef usage filter-----
-
+    //-----end facility country filter-----
 
     //-----name search filter-----
     var name_search = $("#search-name").val().replace("'", "\\'");
