e66b4375c7c0dc6c5253020fcc8a289ad5df890b
Annalisa Minelli <annalisapg@users.noreply.github.com>
2020-01-14 10:57:36 +0100

    data and reference in map.js added

--- js/map.js
+++ js/map.js
@@ -8,7 +8,7 @@ $(window).resize(function () {
 $(function() {
 
   SearchableMapLib.initialize({
-    filePath: 'data/end_DB_blank_FilledUp.csv',
+    filePath: 'data/end_DB_blank_filledUp1.csv',
     fileType: 'csv',
     recordName: 'reef',
     recordNamePlural: 'reefs',
