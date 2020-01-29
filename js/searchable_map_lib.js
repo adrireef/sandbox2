0240e9635f3476b7f100535a3f9e5a4da253b143
Annalisa Minelli <annalisapg@users.noreply.github.com>
2020-01-29 12:15:24 +0100

    Update searchable_map_lib.js

--- js/searchable_map_lib.js
+++ js/searchable_map_lib.js
@@ -571,7 +571,6 @@ var SearchableMapLib = {
 	var newWin=window.open('','Print-Window');
 	newWin.document.open();
 	newWin.document.write('<html><body>'+divToPrint.innerHTML+'</body></html>');
-	newWin.window.print()
 //	newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
 //	newWin.document.close();
 	setTimeout(function(){newWin.close();},10);
