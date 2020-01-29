b62a857147288c80c850315f4c7e6ce7e07a2b6d
Annalisa Minelli <annalisapg@users.noreply.github.com>
2020-01-29 09:46:12 +0100

    Update searchable_map_lib.js

--- js/searchable_map_lib.js
+++ js/searchable_map_lib.js
@@ -570,7 +570,7 @@ var SearchableMapLib = {
 	var divToPrint=document.getElementById('modal-pop');
 	var newWin=window.open('','Print-Window');
 	newWin.document.open();
-	newWin.document.write('<html><body onload="window.print()">'+divToPrint.innerHTML+'</body></html>');
+	newWin.document.write('<html><body onload="window.print()">'+divToPrint.textContent+'</body></html>');
 	newWin.document.close();
 	setTimeout(function(){newWin.close();},10);
 	}
