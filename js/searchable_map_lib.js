68aa03022e77d6841a2497ace3dd5d35e9aa148d
Annalisa Minelli <annalisapg@users.noreply.github.com>
2020-01-31 15:00:27 +0100

    Update searchable_map_lib.js

--- js/searchable_map_lib.js
+++ js/searchable_map_lib.js
@@ -538,7 +538,7 @@ var SearchableMapLib = {
 	  SearchableMapLib.selectIcon();
       SearchableMapLib.modalPop(e.target.feature.properties);
       if (SearchableMapLib.currentPinpoint != '') {
-              map.removeLayer(SearchableMapLib.currentPinpoint);
+              SearchableMapLib.map.removeLayer(SearchableMapLib.currentPinpoint);
         };
 //    getIcon:yellowIcon
     }
